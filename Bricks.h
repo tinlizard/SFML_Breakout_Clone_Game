e2ea183e5bb1d9053829bd9598202de4c4c560fa
tinlizard <38041074+tinlizard@users.noreply.github.com>
2023-11-30 16:52:35 +0100

    Create Bricks.h

---
+++ Bricks.h
@@ -0,0 +1,12 @@
+#pragma once
+#include <SFML/Graphics.hpp>
+#include <iostream>
+
+class Brick {
+	public:
+		Brick(float x, float y, int width, int height,sf::Color color);
+		float x, y;
+		int width, height;
+		sf::Color color;
+};
+std::vector<sf::RectangleShape> drawBricks();
