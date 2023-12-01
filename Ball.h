819d52b9fbc8974770d993e6f45dbebc91ac461d
tinlizard <38041074+tinlizard@users.noreply.github.com>
2023-12-01 09:50:18 +0100

    Update Ball.h

--- Ball.h
+++ Ball.h
@@ -4,8 +4,10 @@
 
 class Ball {
 	public:
-		Ball(float radius, sf::Color color);
-		float radius;
+		Ball(float x, float y, float radius, sf::Color color, float ballOffsetX, float ballOffsetY);
+		float x,y,radius,ballOffsetX,ballOffsetY;
 		sf::Color color;
 };
-sf::CircleShape drawBall();
+sf::CircleShape drawBall(const Ball& ballShape);
+void reverseDirection(float& ballOffsetX, float& ballOffsetY);
+void checkCollisionWithPaddle(sf::RectangleShape& paddle, sf::CircleShape& ball, float& ballOffsetX, float& ballOffsetY);
