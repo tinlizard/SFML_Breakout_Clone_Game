a3e506728137405434371954017fac8ab19d14ef
tinlizard <38041074+tinlizard@users.noreply.github.com>
2023-12-01 09:50:01 +0100

    Update Ball.cpp

--- Ball.cpp
+++ Ball.cpp
@@ -1,15 +1,33 @@
 #include "Ball.h"
 
-Ball::Ball(float radius, sf::Color color) {
+
+Ball::Ball(float x, float y,float radius, sf::Color color,float ballOffsetX, float ballOffsetY) {
 	this->radius = radius;
 	this->color = color;
+    this->x = x;
+    this->y = y;
+    this->ballOffsetX = ballOffsetX;
+    this->ballOffsetY = ballOffsetY;
 }
 
 //function for drawing ball
-sf::CircleShape drawBall() {
-    Ball ballShape(10.f, sf::Color(100, 250, 50));
+sf::CircleShape drawBall(const Ball& ballShape) {
     sf::CircleShape ball(ballShape.radius);
     ball.setFillColor(ballShape.color);
-    ball.setPosition(sf::Vector2f(400.f, 300.f));
+    ball.setPosition(sf::Vector2f(ballShape.x, ballShape.y));
     return ball;
+}
+
+void reverseDirection(float& ballOffsetX, float& ballOffsetY) {
+    ballOffsetX = -ballOffsetX;
+    ballOffsetY = -ballOffsetY;
+}
+
+void checkCollisionWithPaddle(sf::RectangleShape& paddle, sf::CircleShape& ball, float& ballOffsetX, float& ballOffsetY) {
+    if (ball.getGlobalBounds().intersects(paddle.getGlobalBounds())) {
+        int currentX = ball.getPosition().x;
+        int currentY = ball.getPosition().y;
+        ball.setPosition(currentX-20,currentY-20);
+        reverseDirection(ballOffsetX, ballOffsetY);
+    }
 }
