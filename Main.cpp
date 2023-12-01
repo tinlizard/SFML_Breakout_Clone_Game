5dec6a969a107266a2f87c3370e1eb5e41d819cc
tinlizard <38041074+tinlizard@users.noreply.github.com>
2023-12-01 09:50:56 +0100

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -1,5 +1,3 @@
-#include <SFML/Graphics.hpp>
-#include <iostream>
 #include "Ball.h"
 #include "Bricks.h"
 #include "Paddle.h"
@@ -8,18 +6,23 @@
 int main()
 {
     // create the window
-    sf::RenderWindow window(sf::VideoMode(800, 600), "My window");
+    sf::RenderWindow window(sf::VideoMode(800, 600), "Breakout");
     sf::RectangleShape paddle = drawPaddle();
-    sf::CircleShape ball = drawBall();
-    Brick brickShape(50, 50, 70, 20, sf::Color(100, 250, 50));
-    Brick brickShapeTwo(50, 10, 70, 20, sf::Color(100, 250, 50));
+    Ball ballShape(400, 300, 10.f, sf::Color(100, 250, 50),0.05f,0.05f);
+    sf::CircleShape ball = drawBall(ballShape);
+    Brick brickShape(5, 50, 70, 20, sf::Color(100, 250, 50));
+    Brick brickShapeTwo(5, 10, 70, 20, sf::Color(100, 250, 50));
+    Brick brickShapeThree(5, 90, 70, 20, sf::Color(100, 250, 50));
+    Brick brickShapeFour(5, 130, 70, 20, sf::Color(100, 250, 50));
     std::vector<sf::RectangleShape> firstBricksArr = drawBricks(brickShape);
     std::vector<sf::RectangleShape> secondBricksArr = drawBricks(brickShapeTwo);
+    std::vector<sf::RectangleShape> thirdBricksArr = drawBricks(brickShapeThree);
+    std::vector<sf::RectangleShape> fourthBricksArr = drawBricks(brickShapeFour);
     // run the program as long as the window is open
     while (window.isOpen())
     {
         // check all the window's events that were triggered since the last iteration of the loop
-        ball.move(1, 1);
+        ball.move(ballShape.ballOffsetX,ballShape.ballOffsetY);
         sf::Event event;
         while (window.pollEvent(event))
         {
@@ -28,10 +31,10 @@ int main()
                 window.close();
             if (event.type == sf::Event::KeyPressed) {
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::A)) {
-                    paddle.move(-6, 0);
+                    paddle.move(-10, 0);
                 }
                 if (sf::Keyboard::isKeyPressed(sf::Keyboard::D)) {
-                    paddle.move(6, 0);
+                    paddle.move(10, 0);
                 }
             }
         }
@@ -39,12 +42,14 @@ int main()
         window.clear();
         window.draw(paddle);
         //draw each brick in the bricks array
-        for (int i = 0; i < 9; i++) {
+        for (int i = 0; i < 10; i++) {
             window.draw(firstBricksArr[i]);
             window.draw(secondBricksArr[i]);
+            window.draw(thirdBricksArr[i]);
+            window.draw(fourthBricksArr[i]);
         }
         window.draw(ball);
-
+        checkCollisionWithPaddle(paddle, ball,ballShape.ballOffsetX,ballShape.ballOffsetY);
         // end the current frame
         window.display();
     }
