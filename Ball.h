73fec7be80d22e50e155b26dd854910fa084331c
tinlizard <38041074+tinlizard@users.noreply.github.com>
2024-02-06 21:55:47 +0100

    Update Ball.h

--- Ball.h
+++ Ball.h
@@ -1,13 +1,21 @@
 #pragma once
 #include <SFML/Graphics.hpp>
 #include <iostream>
+#include "Bricks.h"
+#include <vector>
 
+//define ball class properties
 class Ball {
 	public:
-		Ball(float x, float y, float radius, sf::Color color, float ballOffsetX, float ballOffsetY);
+		Ball(float x, float y, float radius, sf::Color color, float ballOffsetX, float ballOffsetY, sf::CircleShape ball);
 		float x,y,radius,ballOffsetX,ballOffsetY;
+		bool hasCollidedWithWall;
 		sf::Color color;
+		sf::CircleShape ball;
+		sf::CircleShape drawBall();
+		void reverseDirection();
+		void checkCollisionWithPaddle(const sf::RectangleShape& paddle);
+		void checkCollisionWithBricks(std::vector<std::vector<sf::RectangleShape>>& bricksArray, int& score);
+		void checkBallOffScreen(std::vector<std::vector<sf::RectangleShape>>& bricksArray, int& score, int& lives);
+		bool checkWallCollision();
 };
-sf::CircleShape drawBall(const Ball& ballShape);
-void reverseDirection(float& ballOffsetX, float& ballOffsetY);
-void checkCollisionWithPaddle(sf::RectangleShape& paddle, sf::CircleShape& ball, float& ballOffsetX, float& ballOffsetY);
